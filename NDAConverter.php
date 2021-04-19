85ec7585bcb341dd42d858818c2e51d41b90975d
Andrew Poppe <AndrewBPoppe@gmail.com>
2021-04-19 17:06:45 -0400

    initial

---
+++ NDAConverter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace YaleREDCap\NDAConverter;
+
+/**
+ * Main EM class
+ * 
+ * @author Andrew Poppe
+ */
+class NDAConverter extends \ExternalModules\AbstractExternalModule {
+    
+}
