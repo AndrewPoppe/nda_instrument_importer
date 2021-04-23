014cd79f2c01be58ec4622fc85df26dfde238a18
Andrew Poppe <AndrewBPoppe@gmail.com>
2021-04-23 17:28:22 -0400

    php version almost done

--- src/js/testing.php
+++ src/js/testing.php
@@ -11,6 +11,29 @@ $matchFields = [
 	"Notes"
 ];
 
+$header = [
+	'variable'      => 'Variable / Field Name',
+	'form'          => 'Form Name',
+	'header'        => 'Section Header',
+	'type'          => 'Field Type',
+	'label'         => 'Field Label',
+	'choices'       => 'Choices, Calculations, OR Slider Labels',
+	'note'          => 'Field Note',
+	'validation'    => 'Text Validation Type OR Show Slider Number',
+	'min'           => 'Text Validation Min',
+	'max'           => 'Text Validation Max',
+	'id'            => 'Identifier?',
+	'branching'     => 'Branching Logic (Show field only if...)',
+	'required'      => 'Required Field?',
+	'alignment'     => 'Custom Alignment',
+	'question_num'  => 'Question Number (surveys only)',
+	'matrix_name'   => 'Matrix Group Name',
+	'matrix_rank'   => 'Matrix Ranking?',
+	'annotation'    => 'Field Annotation',
+];
+
+
+
 exit(json_encode(array(
     "type" => "text/csv",
     "data" => convert_all_in_one($payload),
@@ -38,16 +61,23 @@ function throw_error(string $message, int $code = 500) {
  * grab the values from the array keys of the $row parameter. 
  *
  * @param array $row The Array to formatted as CSV.
- * @param array $header The array of explicitly defined header values.
- * @param bool $doHeader Whether to write a header row.
+ * @param array $header An associative array with header id as key and header title as value
+ * @param bool $getHeader Whether to return the header row instead of data.
  * 
  * @return string The formatted CSV string.
  */
-function array_to_csv(array $row, array $header = NULL, bool $doHeader = FALSE) {
+/*function array_to_csv(array $data, bool $getHeader = FALSE) {
     $outstream = fopen("php://temp", 'r+');
-    $header = $header ?? array_keys($arr[0]);
-    $doHeader && fputcsv($outstream, $header, ',', '"');
-    fputcsv($outstream, $row, ',', '"');
+    $data = $getHeader ? array_values($data) : $data;
+    fputcsv($outstream, $data, ',', '"');
+    rewind($outstream);
+    $csv = fgets($outstream);
+    fclose($outstream);
+    return $csv;
+}*/
+function array_to_csv(array $data) {
+    $outstream = fopen("php://temp", 'r+');
+    fputcsv($outstream, $data, ',', '"');
     rewind($outstream);
     $csv = fgets($outstream);
     fclose($outstream);
@@ -107,6 +137,216 @@ function inputIsValid(array $csvArr, array $fields) {
 }
 
 
+function getRanges(array $arr) {
+    return array_map(function($element1) {
+        $bounds = explode('::', $element1);
+        $bounds = array_map(function($bound) {
+            return trim(preg_replace("/NDAR.*/", "", $bound));
+        }, $bounds);
+        if (count($bounds) === 2 && is_numeric($bounds[0]) && is_numeric($bounds[1])){
+            $val1 = intval($bounds[0]);
+            $val2 = intval($bounds[1]);
+            return range(min($val1, $val2), max($val1, $val2));
+        } else {
+            return $bounds;
+        }
+    }, $arr);
+}
+
+
+function reduceRanges(array $arr) {
+    return array_reduce($arr, function($res, $item) {
+        $item = array_map('strval', $item);
+        return array_merge($res, $item);
+    }, []);
+}
+
+
+function parseValueRange(string $vr) {
+    $arr = explode(';', $vr);
+    $ranges = getRanges($arr);
+    $reducedRanges = reduceRanges($ranges);
+    return array_unique($reducedRanges);
+}
+
+
+function clean($val) {
+    return preg_replace('/^[,;\'"\s]*|[,;\'"\s]*$/', '', $val);
+}
+
+function reMatch(string $note, string $parsedVrString, string $re) {
+    preg_match_all($re, $note, $matches, PREG_SET_ORDER);
+    return array_map(function($match) {
+        return array(
+            "key" => clean($match["key"]),
+            "value" => clean($match["val"])
+        );
+    }, $matches);
+}
+
+function delimMatch(string $note, array $parsedVr, string $delimiter) {
+    $split = preg_split("/[${delimiter}]/", $note);
+    $result = array_filter($split, function($el){
+        return strpos($el, '=') !== FALSE;
+    });
+    $result = array_map(function($whole) use ($parsedVr) {
+        $parts = explode('=', $whole);
+        $parts = array_map('trim', $parts);
+        if (in_array($parts[1], $parsedVr)) {
+            $parts = array_reverse($parts);
+        }
+        if (in_array($parts[0], $parsedVr)) {
+            return array(
+                "key" => clean($parts[0]),
+                "value" => clean($parts[1])
+            );
+        }
+        return NULL;
+    }, $result);
+
+    return array_filter($result, function($piece) {
+        return !is_null($piece);
+    });
+}
+
+
+function chooseMatches(array $matches, int $eqs, array $parsedVr) {
+    $pvrl = count($parsedVr);
+    if ($eqs === $pvrl) {
+        $matches = array_filter($matches, function($matches) {
+            return count($matches) === $eqs;
+        })[0];
+    } else {
+        usort($matches, function ($a, $b) {
+            return count($b) - count($a);
+        });
+        $matches = $matches[0];
+        if (count($matches) > 0) {
+
+            // If we have matches with duplicate keys, merge them
+            $matches = array_reduce($matches, function ($acc, $el) {
+                $duplicate = FALSE;
+                foreach ($acc as $i=>$acc_el) {
+                    if ($acc_el["key"] === $el["key"]) {
+                        $acc[$i]["value"] .= "; " . $el["value"];
+                        $duplicate = TRUE;
+                    }
+                }
+                if (!$duplicate) array_push($acc, $el);
+                return $acc;
+            }, []);
+
+            // If we have more values without matches, create them
+            foreach ($matches as $match) {
+                unset($parsedVr[$match["key"]]);
+            }
+            foreach ($parsedVr as $val) {
+                $key = preg_replace('/[^0-9A-Za-z._\-]/', '', $val);
+                if ($key !== "") {
+                    array_push($matches, array(
+                        "key" => $key,
+                        "value" => $val
+                    ));
+                }
+            }
+        } else {
+            foreach ($parsedVr as $i=>$val) {
+                $key = (string) (is_numeric($val) ? (int)$val : intval($i)+1);
+                array_push($matches, array(
+                    "key" => $key,
+                    "value" => $val
+                ));
+            }
+        }
+
+    }
+    $finalMatches = "";
+    $fieldNote = $note;
+    if ($matches) {
+        $finalMatches = array_map(function($match) {
+            return $match["key"] . ", " . $match["value"];
+        }, $matches);
+        natcasesort($finalMatches);
+        $finalMatches = implode(' | ', $finalMatches);
+        $fieldNote = array_map(function($match) {
+            return $match["key"] . " = " . $match["value"];
+        }, $matches);
+        natcasesort($fieldNote);
+        $fieldNote = implode('; ', $fieldNote);
+    }
+
+    return array (
+        "matches" => $finalMatches,
+        "fieldNote" => $fieldNote
+    );
+}
+
+
+function parseNote(string $note, array $parsedVr = []) {
+    $parsedVr = array_map(function($el) {
+        if ($el === "") return "";
+        $result = preg_replace('/[^0-9A-Za-z._\-]/', '', trim($el));
+        if ($result === "") $result = "custom".ord(trim($el));
+        return $result;
+    }, $parsedVr);
+    $parsedVrString = array_map(function($el) {
+        return addcslashes($el, '\\');
+    }, $parsedVr);
+    $parsedVrString = implode('|', $parsedVrString);
+    
+    if ($parsedVrString === "") return "";
+
+    $matchesFore    = reMatch($note, $parsedVrString, "/(?<key>${parsedVrString})\s*=(?<val>(?:(?!(${parsedVrString})\s*=).)*)/");
+    $matchesBack    = reMatch($note, $parsedVrString, "/(?<val>(?:(?!\s*=\s*(${parsedVrString})).)*)\s*=\s*(?<key>${parsedVrString})/");
+    $matches_sc     = delimMatch($note, $parsedVr, ';');
+    $matches_c      = delimMatch($note, $parsedVr, ',');
+    $matchs_scc     = delimMatch($note, $parsedVr, ';,');
+    $allMatches     = [$matchesFore, $matchesBack, $matches_sc, $matches_c, $matchs_scc];
+    $eqs            = substr_count($note, '=');
+
+    $matches = chooseMatches($allMatches, $eqs, $parsedVr);
+    
+
+    return $matches;
+}
+
+
+function parseBounds(string $valueRange, $parsedNote) {
+    $bounds = explode('::', $valueRange);
+    $bounds = array_map(function($e) {
+        return trim(preg_replace('/NDAR.*/', '', $bounds));
+    }, $bounds);
+    if (!$parsedNote && count($bounds) == 2 && is_numeric($bounds[0]) && is_numeric($bounds[1])) {
+        sort($bounds);
+        return $bounds;
+    } else {
+        return [NULL, NULL];
+    }
+}
+
+
+function getFieldType(string $parsedNote) {
+    return $parsedNote ? "text" : "dropdown";
+}
+
+
+function getTextValidation(string $dataType, string $fieldType) {
+    if ($fieldType !== "text") return NULL;
+    switch ($dataType) {
+        case "Date":
+        return "date_mdy";
+            break;
+        case "Integer":
+            return "integer";
+            break;
+        case "Float":
+            return "number";
+            break;
+        default:
+            return NULL;
+	}
+}
+
 /**
  * Create a single data dictionary.
  * 
@@ -133,11 +373,35 @@ function createDataDictionary(array $csvArr, string $form, string $duplicateActi
     }
 
     // Create the Data Dictionary
-    $result = array_map(function($field) {
-        $note = preg_replace('/[\n\r]/', '', $field["Notes"]);
-
+    $result = array_map(function($field) use ($pvrs) {
+        $note               = preg_replace('/[\n\r]/', '', $field["Notes"]);
+        $pvr                = parseValueRange($field["ValueRange"]);
+        $parsed             = parseNote($note, $pvr);
+        $parsedNote         = $parsed->matches;
+        $fieldNote          = $parsed->fieldNote;
+        $bounds             = parseBounds($field["ValueRange"], $parsedNote);
+        $field_type         = getFieldType($parsedNote);
+        $text_validation    = getTextValidation($field["DataType"], $field_type);
+        
         return array (
-            "Notes" => $note
+            "variable"      => $field["ElementName"],
+            "form"          => $form,
+            "header"        => NULL,
+            "type"          => $field_type,
+            "label"         => $field["ElementDescription"],
+            "choices"       => $parsedNote,
+            "note"          => $fieldNote,
+            "validation"    => $text_validation,
+            "min"           => $bounds[0],
+            "max"           => $bounds[1],
+            "id"            => NULL,
+            "branching"     => NULL,
+            "required"      => $field["required"] === "Required" ? "y" : NULL,
+            "alignment"     => NULL,
+            "question_num"  => NULL,
+            "matrix_name"   => NULL,
+            "matrix_rank"   => NULL,
+            "annotation"    => $note
         );
     }, $csvArr);
 
@@ -151,7 +415,7 @@ function convert_all_in_one($fileObject) {
     global $matchFields;
     $fieldArray = [];
     $duplicateAction = $fileObject->duplicateAction ?? "";
-    $header = get_header($fileObject->fileArray[0]->data);
+    //$header = get_header($fileObject->fileArray[0]->data);
     $csvString = array_to_csv($header);
 
     foreach($fileObject->fileArray as $fileData) {
